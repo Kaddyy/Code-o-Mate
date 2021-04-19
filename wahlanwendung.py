00cd4c6cc766d6901da73f6883dad3486a86d166
Kaddy <katrin.kugelmann@hs-augsburg.de>
2021-04-19 09:38:41 +0200

    add "startseite.html"

--- wahlanwendung.py
+++ wahlanwendung.py
@@ -1,14 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 #das ist unsere erste Seite
 @app.route('/')
 def welcome():
-    return "Willkommen!"
-
-
-
+    return render_template('startseite.html')
 
 if __name__ == '__main__':
     app.run()
