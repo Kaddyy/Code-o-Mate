9c365b751f97a0d9e9fbfb2e90566a09ee9ee967
Ulla Moroff <ulla.moroff@hs-augsburg.de>
2021-05-15 15:39:29 +0200

    Create final database

--- wahlanwendung/admin_models.py
+++ wahlanwendung/admin_models.py
@@ -2,6 +2,7 @@ from flask_admin.contrib.sqla import ModelView
 from wahlanwendung import admin, db, models
 
 admin.add_view(ModelView(models.progr_spr, db.session))
-admin.add_view(ModelView(models.sessionergebnis, db.session))
+admin.add_view(ModelView(models.fragenkatalog, db.session))
+admin.add_view(ModelView(models.antwortkatalog, db.session))
 
 models.db.create_all()
--- wahlanwendung/models.py
+++ wahlanwendung/models.py
@@ -10,35 +10,32 @@ class progr_spr(db.Model):
     link2 = db.Column(db.String, unique=False, nullable=True)
     link3 = db.Column(db.String, unique=False, nullable=True)
 
-   # def __init__(self, pk_sprache, beschreibung, absolutes_erg, relatives_erg, link1, link2, link3):
-    #    self.pk_sprache = pk_sprache
-     #   self.beschreibung = beschreibung
-      #  self.absolutes_erg = absolutes_erg
-       # self.relatives_erg = relatives_erg
-        #self.link1 = link1
-        #self.link2 = link2
-        #self.link3 = link3
-
-
-
-class sessionergebnis(db.Model):
+class fragenkatalog(db.Model):
     __table_args__ = {'extend_existing': True}
-    pk_session_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    fk_java = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_python = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_swift = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_cplusplus = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_csharp = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_javascript = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_matlab = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_go = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_htmlcss = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_sql = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_php = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_r = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_typescript = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_kotlin = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
-    fk_abap = db.Column(db.Integer, db.ForeignKey('progr_spr.pk_sprache'), nullable=False, default=0)
+    pk_frage_id = db.Column(db.Integer, primary_key=True)
+    frage = db.Column(db.String, unique=False, nullable=False)
+
+
+class antwortkatalog(db.Model):
+    __table_args__ = {'extend_existing': True}
+    pk_antwort_id = db.Column(db.String, primary_key=True)
+    fragennummer = db.Column(db.Integer, unique=False, nullable=False)
+    antwort = db.Column(db.String, unique=False, nullable=False)
+    java = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    python = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    swift = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    cplusplus = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    csharp = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    javascript = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    matlab = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    go = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    htmlcss = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    sql = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    php = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    r = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    typescript = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    kotlin = db.Column(db.Integer, unique=False, nullable=False, default=0)
+    abap = db.Column(db.Integer, unique=False, nullable=False, default=0)
 
 
 
