d74c06e9d280ad7c9f1bb6f38f24d3d9e8a5b989
Kaddy <katrin.kugelmann@hs-augsburg.de>
2021-05-20 14:20:11 +0200

    add javascript. Button: "Weiter" loads new content (question)

--- wahlanwendung/static/fragenkatalog.js
+++ wahlanwendung/static/fragenkatalog.js
@@ -1,17 +1,16 @@
 var counter = 0;
 
 function neueFrage() {
-    text = "ich will FRAGEN!";
+    fragentext = "ich will FRAGEN!";
+    antworttext1 = "ich bin anw1";
+    antworttext2 = "ich bin anw2";
+    antworttext3 = "ich bin anw3";
+    antworttext4 = "ich bin anw4";
     counter++;
-    var jetzt = text + counter;
-    document.querySelector('#frageNormal').innerText = jetzt;
+
+    document.querySelector('#frageNormal').innerText = fragentext  + counter;
+    document.querySelector('#label_01').innerText = antworttext1 + counter;
+    document.querySelector('#label_02').innerText = antworttext2 + counter;
+    document.querySelector('#label_03').innerText = antworttext3 + counter;
+    document.querySelector('#label_04').innerText = antworttext4 + counter;
 }
-
-/*fragentext = "Frage2";
-
-var updateFrage = function(fragentext) {
-    document.querySelector('#frageNormal').innerText += fragentext;
-}
-
-updateFrage(fragentext);
-*/
