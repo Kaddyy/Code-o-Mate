cb50850ffcb9b3db4f3ccf0d9999463cdf4504da
Ulla Moroff <ulla.moroff@hs-augsburg.de>
2021-05-31 15:09:29 +0200

    hide None-radios and the non used button

--- wahlanwendung/static/fragenkatalog.js
+++ wahlanwendung/static/fragenkatalog.js
@@ -1,5 +1,21 @@
-alert("Ich bin JSSS");
-function checkTonio(){
+    whatauchimmer = document.querySelector("#label_03").innerText;
+    if (whatauchimmer == "None") {
+    document.querySelector("#label_03").hidden = true;
+    document.querySelector("#a01_3").hidden = true;
+    }
+    whatauchimmer2 = document.querySelector("#label_04").innerText;
+    if (whatauchimmer2 == "None") {
+    document.querySelector("#label_04").hidden = true;
+    document.querySelector("#a01_4").hidden = true;
+    }
+    letzteFrage = document.querySelector("#frage").innerText;
+    if (letzteFrage != "Hast du Interesse daran Roboter zu programmieren?"){
+    document.querySelector("#auswertung").hidden = true;
+    } else{
+    document.querySelector("#weiterbtn").hidden = true;
+    }
+
+/*function checkTonio(){
     //checkAnswer3 = antworten.antwort3;
     document.querySelector("#label_03").innerText = element;
     if (element == "Ulla"){
@@ -8,4 +24,4 @@ function checkTonio(){
     } else {
         document.querySelector("#label_03").innerText = "Kaddy";
     }
-}
+}*/
