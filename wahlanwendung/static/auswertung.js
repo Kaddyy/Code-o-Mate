1b2d586a4ac72c866564f058295758d017bc3a2b
Kaddy <katrin.kugelmann@hs-augsburg.de>
2021-06-13 17:21:41 +0200

    fix structur; add hover-effect (SVG)

---
+++ wahlanwendung/static/auswertung.js
@@ -0,0 +1,10 @@
+/*
+gsap.set("KachelHintergrundID");
+
+document.getElementById("testID").addEventListener("click", function() {
+    gsap.to("KachelHintergrundID", {
+        fill: red,
+        overwrite: true,
+        duration: 1
+    });
+    });
